d3e3748c39c8bf2a753920fcde8df4e18020f550
Ken Sodemann <ken@ionic.io>
2021-08-19 15:18:57 -0500

    feat(app): install the platforms

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.sacandidatesample',
   appName: 'sa-candidate-sample',
   webDir: 'dist',
   bundledWebRuntime: false
